6f050bac8e131b4da12c415e4672069a9f7a0355
Oliver <schmahl103@gmail.com>
2022-09-28 21:27:25 +0200

    AudioScript updated

--- Immunity Game/Assets/Scripts/AudioScript.cs
+++ Immunity Game/Assets/Scripts/AudioScript.cs
@@ -2,19 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioScript : MonoBehaviour
 {
-
-    public static AudioScript instance;
+    private static AudioScript instance = null;
+    
+    public static AudioScript Instance
+    {
+        get { return instance; }
+    }
     void Awake()
     {
-        if (instance != null)
+        if (instance != null && instance != this)
         {
             Destroy(gameObject);
+            return;
         }
-
+        Debug.Log("Scene changed");
         instance = this;
-        DontDestroyOnLoad(transform.gameObject);
+        DontDestroyOnLoad(gameObject);
     }
 }
