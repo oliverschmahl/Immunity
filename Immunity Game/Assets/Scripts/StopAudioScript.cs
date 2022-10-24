8d5dfa161b684aa0f49aa4c75cb7117d26a435ca
Oliver <schmahl103@gmail.com>
2022-10-24 16:04:09 +0200

    Removed StopAudioScript from Main menu

--- Immunity Game/Assets/Scripts/StopAudioScript.cs
+++ Immunity Game/Assets/Scripts/StopAudioScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +8,17 @@ public class StopAudioScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (AudioScript.Instance.gameObject)
-        {
-            Destroy(AudioScript.Instance.gameObject);
+        try {
+            bool audioScriptExists = AudioScript.Instance.gameObject != null;
+            if (audioScriptExists)
+            {
+                Destroy(AudioScript.Instance.gameObject);
+            }
+        }       
+        catch (NullReferenceException ex) {
+            Debug.Log("There is no AudioScript to stop");
         }
+        
+        
     }
-
 }
